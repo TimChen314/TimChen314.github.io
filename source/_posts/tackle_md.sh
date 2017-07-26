029030afc82d572d4b3890049db5d5fd6fb670d7
TimChen314 <chentao314@163.com>
2017-07-26 21:30:52 +0800

    update article

--- source/_posts/tackle_md.sh
+++ source/_posts/tackle_md.sh
@@ -2,7 +2,7 @@
 
 hostname=$(hostname)
 if [ "$hostname" = "TaodeMBP" ];then
-    file_list="人工智能简介.md hdf5.md git.md"
+    file_list="Mou_人工智能简介.md Mou_hdf5.md Mou_git.md"
     cd /Users/Aether/Documents/md/ && cp $file_list $HEXOMD && cd $HEXOMD
 fi
 
